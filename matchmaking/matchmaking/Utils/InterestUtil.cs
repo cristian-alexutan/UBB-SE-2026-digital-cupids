514caff877d8baf6a290b8a0f73ebde54ab0543d
davidberbecar30 <berbecardavid681@gmail.com>
2026-03-25 21:43:44 +0300

    csv din txt

--- matchmaking/matchmaking/Utils/InterestUtil.cs
+++ matchmaking/matchmaking/Utils/InterestUtil.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
+
+[assembly: InternalsVisibleTo("matchmaking.Tests")]
 namespace matchmaking.Utils
 {
     internal class InterestUtil
@@ -16,16 +19,19 @@ namespace matchmaking.Utils
         {
             interests = new List<String>();
             string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            interestsFile = Path.Combine(baseDirectory, "interests.txt");
+            interestsFile = Path.Combine(baseDirectory, "interests.csv");
 
             if (File.Exists(interestsFile))
             {
                 String allLines = File.ReadAllText(interestsFile);
-                interests = allLines.Split(',').Select(i=>i.Trim().ToLower()).Where(i=>!string.IsNullOrEmpty(i)).ToList();
+                interests = File.ReadAllLines(interestsFile)
+                                    .Select(i => i.Trim().ToLower())
+                                    .Where(i => !string.IsNullOrEmpty(i))
+                                    .ToList();
             }
             else
             {
-                throw new Exception("The file doesn't exist");
+                throw new FileNotFoundException("The file doesn't exist");
             }
         }
 
