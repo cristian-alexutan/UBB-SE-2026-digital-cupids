b4f9c750d4f9f29503a4e296fdf47c9398867515
bocaraluca <raluca.boca@stud.ubbcluj.ro>
2026-03-25 21:18:17 +0200

    Created Photo domain class

---
+++ matchmaking/matchmaking/Domain/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matchmaking.Domain
+{
+    internal class Photo
+    {
+        public int PhotoId { get; set; }
+        public int UserId { get; set; }
+        public string? Location { get; set; }
+
+        public Photo() { }
+
+        public Photo(int photoId, int userId, string location)
+        {
+            PhotoId = photoId;
+            UserId = userId;
+            Location = location;
+        }
+    }
+}
